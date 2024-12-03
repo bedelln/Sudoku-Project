c54f4de9d57db91a905b2c2988569f1d12ac5d08
Annabelladududu <duy@ufl.edu>
2024-12-02 22:17:11 -0500

    Update sudoku.py
    add global constants to sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+
+#set colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 95, 0)
+
+#set size of the page
+WIDTH = 810
+HEIGHT = 972
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sudoku")
